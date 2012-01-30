26fa2ebe363b78fedfe986a275ba049d41bfe110
Barry Lapthorn <barry@lapthorn.net>
2012-01-30 13:37:10 +0000

    Simple example of MVVM Light.

---
+++ MvvmLightTest/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿//  Example code only, feel free to copy and re-use.
+
+using System.Windows;
+
+namespace MvvmLightTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MvvmLightTest/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿//  Example code only, feel free to copy and re-use.
+
+using System.Windows;
+
+namespace MvvmLightTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
